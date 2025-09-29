098693e4680be5ff1d41ae565cdd9b8b356293b5
rakshith <rakshithg35@gmail.com>
2025-09-29 21:31:24 +0530

    add spring structure

---
+++ src/main/java/com/rakshith/stocksbackend/StocksTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rakshith.stocksbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rakshith/stocksbackend/StocksTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakshith.stocksbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
